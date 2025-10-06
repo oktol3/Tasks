5edf49232e812a52e30511f5dd66a907a82e1314
Котов Олег Тки-141 <159231998+oktol3@users.noreply.github.com>
2025-10-06 11:20:05 +0300

    1.2.c

---
+++ 1.2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+/**
+ * @brief Ввод радиуса
+ * @return Радиус шара
+ */
+double value();
+
+/**
+ * @brief Проверка Правильности введеных данных
+ * @param value - введенное значение
+ */
+void checkValue(const double value);
+
+/**
+ * @brief Точка Входа
+ * @return 0, если программма выполнена корректно
+ */
+int main()
+{
+	printf("A = ");
+	const double A = value();
+	checkValue(A);
+	printf("S = ");
+	const double N = value();
+	checkValue(N);
+	printf("S = %.2lf\n", (N / 100.0) * A * A);
+	printf("P = %.2lf\n", (N / 100.0) * A + A);
+}
+
+double value()
+{
+	double value = 0;
+	if (!scanf_s("%lf", &value))
+	{
+		printf("ERROR");
+		abort();
+	}
+	return value;
+}
+
+void checkValue(const double value)
+{
+	if (value <= 0)
+	{
+		printf("Value have to be positive");
+		abort();
+	}
+}
