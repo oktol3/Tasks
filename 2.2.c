7f4fd3fb314b6766836ca3f60fb22642e46d464e
Котов Олег Тки-141 <159231998+oktol3@users.noreply.github.com>
2025-12-07 23:31:09 +0300

    Update 2.2.c

--- 2.2.c
+++ 2.2.c
@@ -3,31 +3,31 @@
 #include <stdlib.h>
 #include <float.h>
 
-
 /**
  * @brief считывает вещественное значение с клавиатуры с проверкой ввода
  * @return возвращает считанное значение
  */
 double getValue();
+
 /**
- * @brief Рассчет по заданной функции 
+ * @brief Рассчет по заданной функции
  * @param a - заданная константа - 2.5
  * @param x - переменная
  * @return 0, если программа завершена успешно
  */
-double function(const int a, const int x);
+double function(const double a, const double x);
+
 /**
  * @brief Точка входа
  * @return 0, если программа завершена успешно
  */
-
 int main()
 {
-	const double a = 2.5;
+    const double a = 2.5;
     printf("Enter x ");
     const double x = getValue();
-	printf("function = %.2lf", function(x, a));
-	return 0;
+    printf("y = %.2lf", function(a, x));
+    return 0;
 }
 
 double getValue()
@@ -42,15 +42,15 @@ double getValue()
 }
 
 
-double function(const int a, const int x)
+double function(const double a, const double x)
 {
-
     if (!(x>a))
-    {
+    {   
         return x * sin(a) * x;
+        
     }
     else
     {
-        return x * (x - a) ^ (1 / 3);
+        return x * pow((x-a), 1.0 / 3.0);
     }
 }
