e18d4628f02813241a542f6d9d8e3a442b127906
Котов Олег Тки-141 <159231998+oktol3@users.noreply.github.com>
2026-05-14 17:46:56 +0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -14,6 +14,7 @@ private:
         @brief Точка В
     **/
     point B;
+    
 public:
     /**
        @brief конструктор
@@ -21,5 +22,46 @@ public:
        @param B - точка B
     **/
     vector(const point& X, const point& Y);
+    /**
+        @breid сравнение векторов, равны или нет 
+        @param вектор с которым сравниваем
+        @return True или False
+    **/
+    bool equally(const vector vector);
+    /**
+     * @breif сложение векторов
+     * @param vector - вектор с которым происходит сложение
+     * @return вектор полученный сложением
+     */
+    vector addition(const vector vector);
+    /**
+     * @breif вычитание векторов
+     * @param vector - вектор который вычитают
+     * @return вектор полученный вычитанием
+     */
+    vector subtraction(vector vector);
+    /**
+     * @breif умножение вектора на скаляр
+     * @param а - вещественное число, которое умножают на вектор
+     * @return вектор умноженный на число
+     */
+    vector multiply(const double a);
+    /**
+        @breif Координата вектора i
+        @return вещественное число
+     **/
+    double i();
+    /**
+        @breif Координата вектора j
+        @return вещественное число
+     **/
+    double j();
+    /**
+        @breif Координата вектора k
+        @return вещественное число
+     **/
+    double k();
 };
+
+
 
