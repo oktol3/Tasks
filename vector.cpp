b0856abeae9dfe7f4dda9d079cb69749c98a9c6d
Котов Олег Тки-141 <159231998+oktol3@users.noreply.github.com>
2026-05-20 16:11:30 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,46 +1,118 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <float.h>
 #include "vector.h"
 #include "point.h"
 
-vector::vector(const point& A, const point& B) :A(A), B(B)
+vector::vector(const point& X, const point& Y)
+    : A(X), B(Y) {}
+
+vector::vector(const vector& other)
+    : A(other.A)
+    , B(other.B) 
 {
-
+    std::cout << "[DEBUG] vector copy constructor called" << std::endl;
 }
 
-double vector::i(){
-	return (B.x() - A.x());
-}
-double vector::j(){
-	return (B.y() - A.y());
-}
-double vector::k(){
-	return (B.z() - A.z());
-}
-
-bool vector::equally(vector vector)
+vector& vector::operator=(const vector& other)
 {
-	return bool(abs(vector.i() - i()) < DBL_EPSILON && abs(vector.j() - j()) < DBL_EPSILON && abs(vector.k() - k()) < DBL_EPSILON);
+    std::cout << "[DEBUG] vector copy assignment called" << std::endl;
+
+    if (this != &other)
+    {
+        A = other.A;
+        B = other.B;
+    }
+
+    return *this;
 }
 
-vector vector::addition(vector vect)
+
+vector::vector(vector&& other) noexcept
+    : A(std::move(other.A))
 {
-	point A = point(0.0, 0.0, 0.0);
-	point B = point(i() + vect.i(), j() + vect.j(), k() + vect.k());
-	return vector(A, B);
+    std::cout << "[DEBUG] vector move constructor called" << std::endl;
+
 }
 
-vector vector::subtraction(vector vect)
+vector& vector::operator=(vector&& other) noexcept
 {
-	point A = point(0.0, 0.0, 0.0);
-	point B = point(i() - vect.i(), j() - vect.j(), k() - vect.k());
-	return vector(A, B);
+    std::cout << "[DEBUG] vector move assignment called" << std::endl;
+
+    if (this != &other)
+    {
+        A = std::move(other.A);
+        B = std::move(other.B);
+    }
+
+    return *this;
 }
 
-vector vector::multiply(const double a)
+vector::~vector()
 {
-	point A = point(0.0, 0.0, 0.0);
-	point B = point(i() * a, j() * a, k() * a);
-	return vector(A, B);
+    std::cout << "[DEBUG] vector destructor called" << std::endl;
+
+}
+
+bool vector::operator==(const vector& other) const
+{
+    return (std::abs(i() - other.i()) < DBL_EPSILON &&
+        std::abs(j() - other.j()) < DBL_EPSILON &&
+        std::abs(k() - other.k()) < DBL_EPSILON);
+}
+
+bool vector::operator!=(const vector& other) const
+{
+    return !(*this == other);
+}
+
+vector vector::operator+(const vector& other) const
+{
+    double new_i = i() + other.i();
+    double new_j = j() + other.j();
+    double new_k = k() + other.k();
+
+    point new_point(new_i, new_j, new_k);
+
+    return vector(point(0, 0, 0), new_point);
+}
+
+vector vector::operator-(const vector& other) const
+{
+    double new_i = i() - other.i();
+    double new_j = j() - other.j();
+    double new_k = k() - other.k();
+
+    point new_point(new_i, new_j, new_k);
+    return vector(point(0, 0, 0), new_point);
+}
+
+vector vector::operator*(double scalar) const
+{
+    double new_i = i() * scalar;
+    double new_j = j() * scalar;
+    double new_k = k() * scalar;
+
+    point new_point(new_i, new_j, new_k);
+    return vector(point(0, 0, 0), new_point);
+}
+
+vector operator*(double scalar, const vector& v)
+{
+    return v * scalar;
+}
+
+double vector::i() const
+{
+    return B.x() - A.x();
+}
+
+double vector::j() const
+{
+    return B.y() - A.y();
+}
+
+double vector::k() const
+{
+    return B.z() - A.z();
 }
