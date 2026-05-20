2c45f5f2d86dba1475d75308db4794db7376b202
Котов Олег Тки-141 <159231998+oktol3@users.noreply.github.com>
2026-05-20 16:12:03 +0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,67 +1,120 @@
 #pragma once
 #include "point.h"
+
 /**
-    @brief класс вектор
-**/
+ * @brief   класс Вектор в трехмерном пространстве
+ * @details Вектор задается двумя точками класса point
+ */
 class vector
 {
 private:
     /**
-        @brief Точка А
-    **/
+     * @breif начальная точка вектора
+     */
     point A;
     /**
-        @brief Точка В
-    **/
+     * @breif конечная точка вектора
+     */
     point B;
-    
+
 public:
+
     /**
-       @brief конструктор
-       @param A - точка A
-       @param B - точка B
-    **/
-    vector(const point& X, const point& Y);
-    /**
-        @breid сравнение векторов, равны или нет 
-        @param вектор с которым сравниваем
-        @return True или False
-    **/
-    bool equally(const vector vector);
-    /**
-     * @breif сложение векторов
-     * @param vector - вектор с которым происходит сложение
-     * @return вектор полученный сложением
+     * @brief   Конструктор с параметрами
+     * @param   X - начальная точка вектора
+     * @param   Y - конечная точка вектора
      */
-    vector addition(const vector vector);
+    vector(const point& X = point(0, 0, 0), const point& Y = point(0, 0, 0));
+
     /**
-     * @breif вычитание векторов
-     * @param vector - вектор который вычитают
-     * @return вектор полученный вычитанием
+     * @brief   Конструктор копирования
+     * @param   other - ссылка на копируемый объект
      */
-    vector subtraction(vector vector);
+    vector(const vector& other);
+
     /**
-     * @breif умножение вектора на скаляр
-     * @param а - вещественное число, которое умножают на вектор
-     * @return вектор умноженный на число
+     * @brief   Оператор присваивания копированием
+     * @param   other - ссылка на присваиваемый объект
+     * @return  ссылка на текущий объект (для цепочечных операций)
      */
-    vector multiply(const double a);
+    vector& operator=(const vector& other);
+
     /**
-        @breif Координата вектора i
-        @return вещественное число
-     **/
-    double i();
+     * @brief   Конструктор перемещения
+     * @param   other - ссылка на rvalue перемещаемого объекта
+     */
+    vector(vector&& other) noexcept;
+
     /**
-        @breif Координата вектора j
-        @return вещественное число
-     **/
-    double j();
+     * @brief   Оператор присваивания перемещением
+     * @param   other - ссылка на rvalue перемещаемого объекта
+     * @return  ссылка на текущий объект
+     */
+    vector& operator=(vector&& other) noexcept;
+
     /**
-        @breif Координата вектора k
-        @return вещественное число
-     **/
-    double k();
+     * @brief   Деструктор
+     */
+    ~vector();
+
+    /**
+     * @brief   Оператор равенства векторов
+     * @param   other - ссылка на сравниваемый вектор
+     * @return  true - векторы равны, false - не равны
+     */
+    bool operator==(const vector& other) const;
+
+    /**
+     * @brief   Оператор неравенства векторов
+     * @param   other - ссылка на сравниваемый вектор
+     * @return  true - векторы не равны, false - равны
+     */
+    bool operator!=(const vector& other) const;
+
+    /**
+     * @brief   Оператор сложения векторов
+     * @param   other - ссылка на второй вектор
+     * @return  новый вектор, равный сумме текущего и other
+     */
+    vector operator+(const vector& other) const;
+
+    /**
+     * @brief   Оператор вычитания векторов
+     * @param   other - ссылка на вычитаемый вектор
+     * @return  новый вектор, равный разности текущего и other
+     */
+    vector operator-(const vector& other) const;
+
+    /**
+     * @brief   Оператор умножения вектора на скаляр
+     * @param   scalar - вещественное число
+     * @return  новый вектор, умноженный на scalar
+     */
+    vector operator*(double scalar) const;
+
+    /**
+     * @brief   Дружественная функция для умножения скаляра на вектор
+     * @param   scalar - вещественное число
+     * @param   v      - ссылка на вектор
+     * @return  вектор, умноженный на скаляр
+     */
+    friend vector operator*(double scalar, const vector& v);
+
+    /**
+     * @brief   Получить координату X (i) вектора
+     * @return  координата по оси X (разность B.x - A.x)
+     */
+    double i() const;
+
+    /**
+     * @brief   Получить координату Y (j) вектора
+     * @return  координата по оси Y (разность B.y - A.y)
+     */
+    double j() const;
+
+    /**
+     * @brief   Получить координату Z (k) вектора
+     * @return  координата по оси Z (разность B.z - A.z)
+     */
+    double k() const;
 };
-
-
-
